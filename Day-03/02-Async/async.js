a87f78ce309f9f45f9b65cbb6968721f677f3586
tkmagesh <tkmagesh77@gmail.com>
2015-09-20 12:32:34 +0530

    bugTracker - Server - Seed

--- Day-03/02-Async/async.js
+++ Day-03/02-Async/async.js
@@ -12,19 +12,40 @@ function addSyncClient(x,y){
     console.log("[SC] result = ", result);
 }
 
-/* Async*/
+/* Async - using callback*/
 
-function addAsync(x,y){
+function addAsync(x,y, onResult){
     console.log("[SP] processing ", x , " and ", y);
     setTimeout(function(){
         var result = x + y;
         console.log("[SP] returning result");
-        return result;
+        onResult(result);
     },3000);
 }
 
 function addAsyncClient(x,y){
     console.log("[SC] trigger add");
-    var result = addAsync(x,y);
-    console.log("[SC] result = ", result);
+    addAsync(x,y, function(result){
+        console.log("[SC] result = ", result);
+    });
 }
+
+/* Async - using promise*/
+
+function addAsync(x,y){
+    console.log("[SP] processing ", x , " and ", y);
+    var promise = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            var result = x + y;
+            console.log("[SP] returning result");
+            resolve(result);
+        },3000);
+    });
+    return promise;
+}
+
+
+
+
+
+
